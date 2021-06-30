10d1c60332ea1001684ab698420341ddc1257970
Dyij <qtcutie@kakao.com>
2021-06-30 20:24:06 +0900

    delete dirty eval function

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const parser = require('@dimigo/excel')
 const day = ['mon', 'tue', 'wed', 'thurs', 'fri', 'sat', 'sun']
 // 이 코드는 안보는게 좋아요... 하드코딩의 결정체거든요 하하하
-// eval로 떡칠좀 해봤어요 전 js가 뭔지도 몰라서(?)
 
 async function excel(path) {
     const data = await parser.parseXlsx(path)
@@ -10,9 +9,9 @@ async function excel(path) {
             let i = 0
             for(const meal of data[4+j]){
                 if(!meal.includes('조 식') && !meal.includes('중 식') && !meal.includes('석 식') && meal!=='' && meal!=='*' && meal!=='<국없는날>'){
-                    eval(`result.${day[i]}.breakfast.push("${meal.replace(/\d+/g, '').replaceAll('\(','').replaceAll('\)','').replaceAll('.','').replaceAll('ㆍ','')}")`)
-                    if(eval(`result.${day[i]}.breakfast.indexOf('종과일')`) != -1) eval(`result.${day[i]}.breakfast[result.${day[i]}.breakfast.indexOf('종과일')] = "2종과일"`)
-                    if(eval(`result.${day[i]}.breakfast.indexOf('종시리얼')`) != -1) eval(`result.${day[i]}.breakfast[result.${day[i]}.breakfast.indexOf('종시리얼')] = "2종시리얼"`)
+                    result[day[i]].breakfast.push(meal.replace(/\d+/g, '').replaceAll('\(','').replaceAll('\)','').replaceAll('.','').replaceAll('ㆍ',''))
+                    if(result[day[i]].breakfast.indexOf('종과일') != -1) result[day[i]].breakfast[result[day[i]].breakfast.indexOf('종과일')] = "2종과일"
+                    if(result[day[i]].breakfast.indexOf('종시리얼') != -1) result[day[i]].breakfast[result[day[i]].breakfast.indexOf('종시리얼')] = "2종시리얼"
                     i++
                 }
             }
@@ -21,7 +20,7 @@ async function excel(path) {
             let i = 0
             for(const meal of data[23+j]){
                 if(!meal.includes('조 식') && !meal.includes('중 식') && !meal.includes('석 식') && meal!=='' && meal!=='*' && meal!=='<국없는날>'){
-                    eval(`result.${day[i]}.lunch.push("${meal.replace(/\d+/g, '').replaceAll('\(','').replaceAll('\)','').replaceAll('.','').replaceAll('ㆍ','')}")`)
+                    result[day[i]].lunch.push(meal.replace(/\d+/g, '').replaceAll('\(','').replaceAll('\)','').replaceAll('.','').replaceAll('ㆍ',''))
                     i++
                 }
             }
@@ -30,7 +29,7 @@ async function excel(path) {
             let i = 0
             for(const meal of data[34+j]){
                 if(!meal.includes('조 식') && !meal.includes('중 식') && !meal.includes('석 식') && meal!=='' && meal!=='*' && meal!=='<국없는날>'){
-                    eval(`result.${day[i]}.dinner.push("${meal.replace(/\d+/g, '').replaceAll('\(','').replaceAll('\)','').replaceAll('.','').replaceAll('ㆍ','')}")`)
+                    result[day[i]].dinner.push(meal.replace(/\d+/g, '').replaceAll('\(','').replaceAll('\)','').replaceAll('.','').replaceAll('ㆍ',''))
                     i++
                 }
             }
